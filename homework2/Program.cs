5f108e1535732a3a60ddd9f37b8f4ff21136bf3f
IvanovVladislav <ivanov2703@mail.ru>
2022-08-04 19:00:12 +0300

    add homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Задача 50 ");
+Console.WriteLine(".......................................................... ");
+Console.WriteLine();
+
+Console.WriteLine("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [7,7];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет ");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n}-ой строки и {m}-го столбца ==> [{numbers[n-1,m-1]}]");
+}
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-10, 10);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "; ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
